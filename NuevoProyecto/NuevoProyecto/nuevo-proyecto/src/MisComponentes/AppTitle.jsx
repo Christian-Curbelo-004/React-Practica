20f3847e471af59ebba6baee4d2fd66db9ef76b6
Christian Curbelo <{{your_email}}>
2026-04-22 22:54:02 -0300

    subiendo nuevo proyecto en react

---
+++ NuevoProyecto/NuevoProyecto/nuevo-proyecto/src/MisComponentes/AppTitle.jsx
@@ -0,0 +1,10 @@
+import CounterButton from "./CounterButton";
+
+function AppTitle({ title, children }) {
+    return <div>
+        <h1>{title}</h1>
+        {children}
+    </div>;
+}
+
+export default AppTitle;
